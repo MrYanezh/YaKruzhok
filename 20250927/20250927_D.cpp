af92ce397b621c915ef058a7aa886b4f625e417b
MrYanezh <Tokarev.eugene@gmail.com>
2025-10-06 23:18:05 +0300

    20250927/20250927_D.cpp - Инит комит_функция не работает!

---
+++ 20250927/20250927_D.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    int n = 0;
+    int i = 0;
+    cin >> n;
+    cout << endl;
+    system("pause");
+    return 0;
+}
